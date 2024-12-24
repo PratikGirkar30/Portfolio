c1f4c3498cb15bf665b41b24d39927c219c10cf4
Pratik Girkar <pratik.girkar30@gmail.com>
2024-12-24 12:47:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,4 @@
-import os
 from flask import Flask , render_template ,request ,redirect
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import update
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
-    f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db= SQLAlchemy(app)
-
-class messages(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=False)
-    email = db.Column(db.String(255), unique=False)
-    message = db.Column(db.String(255), unique=False)
-
-
-@app.route('/submit' , methods=['GET', 'POST'])
-def Submit():
-    if(request.method=='POST'):
-        name=request.form['name']
-        email=request.form['email']
-        message=request.form['message']
-        new_con = messages(name=name,email=email,message=message)
-        db.session.add(new_con)
-        db.session.commit()
-        return redirect(url_for('Hello_World'))
-    return render_template('index.html')  
-
 
 @app.route('/')
 def Hello_World():
